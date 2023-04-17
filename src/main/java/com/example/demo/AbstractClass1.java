652024c3e38cdb5696cb2cf1f515d42f01dc952c
Mirado <hei.mirado@gmail.com>
2023-04-17 16:39:35 +0300

    add Class Concret

---
+++ src/main/java/com/example/demo/AbstractClass1.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public abstract class AbstractClass1 {
+    ConcretDependance1 concretDependance1;
+    ConcretDependance2 concretDependance2;
+
+}
---
+++ src/main/java/com/example/demo/ConcretClass2.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConcretClass2 extends AbstractClass1{
+}
