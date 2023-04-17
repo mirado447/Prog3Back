480197b3df1bca130eaf2d41fc9d5ba4209b41c9
Mirado <hei.mirado@gmail.com>
2023-04-17 16:27:27 +0300

    initialisation

---
+++ src/main/java/com/example/demo/Prog3BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog3BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prog3BackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/Prog3BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog3BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
